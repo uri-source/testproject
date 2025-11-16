de710b07974077bd8da82edb8f78cc12240742c3
uri-source <uri@gotribal.ai>
2025-11-16 15:38:04 +0200

    Service360 React - Combined Apex hooks from 1 classes

---
+++ d8e84dbe-0077-4a3d-8163-4a30e4a9627e/compiled/apex/combined/apex-hooks.js
@@ -0,0 +1,42 @@
+// AUTO-GENERATED. DO NOT EDIT.
+import useApexClass from '../useApexClass';
+import { useMemo } from 'react';
+
+export function useApexSER_ServiceDashboardController(bridge) {
+  const apexClass = useApexClass(bridge, 'SER_ServiceDashboardController');
+  return useMemo(() => ({
+    getDashboardData: (timeFrame) => apexClass.mutate('getDashboardData', { timeFrame: timeFrame }, { refresh: ['getDashboardData'] }),
+    getDashboardData: (timeFrame) => apexClass.mutate('getDashboardData', { timeFrame: timeFrame }, { refresh: ['getDashboardData'] }),
+    getDashboardData: (timeFrame) => apexClass.mutate('getDashboardData', { timeFrame: timeFrame }, { refresh: ['getDashboardData'] }),
+    getDashboardData: (timeFrame) => apexClass.mutate('getDashboardData', { timeFrame: timeFrame }, { refresh: ['getDashboardData'] }),
+    getDashboardData: (timeFrame) => apexClass.mutate('getDashboardData', { timeFrame: timeFrame }, { refresh: ['getDashboardData'] }),
+    getDashboardData: (timeFrame) => apexClass.mutate('getDashboardData', { timeFrame: timeFrame }, { refresh: ['getDashboardData'] }),
+    getDashboardData: (timeFrame) => apexClass.mutate('getDashboardData', { timeFrame: timeFrame }, { refresh: ['getDashboardData'] }),
+    getDashboardData: (timeFrame) => apexClass.mutate('getDashboardData', { timeFrame: timeFrame }, { refresh: ['getDashboardData'] }),
+    getDashboardData: (timeFrame) => apexClass.mutate('getDashboardData', { timeFrame: timeFrame }, { refresh: ['getDashboardData'] }),
+    getDashboardData: (timeFrame) => apexClass.mutate('getDashboardData', { timeFrame: timeFrame }, { refresh: ['getDashboardData'] }),
+    getDashboardData: (timeFrame) => apexClass.mutate('getDashboardData', { timeFrame: timeFrame }, { refresh: ['getDashboardData'] }),
+    getDashboardData: (timeFrame) => apexClass.mutate('getDashboardData', { timeFrame: timeFrame }, { refresh: ['getDashboardData'] }),
+    getDashboardData: (timeFrame) => apexClass.mutate('getDashboardData', { timeFrame: timeFrame }, { refresh: ['getDashboardData'] }),
+    getDashboardData: (timeFrame) => apexClass.mutate('getDashboardData', { timeFrame: timeFrame }, { refresh: ['getDashboardData'] }),
+    getDashboardData: (timeFrame) => apexClass.mutate('getDashboardData', { timeFrame: timeFrame }, { refresh: ['getDashboardData'] }),
+    getDashboardData: (timeFrame) => apexClass.mutate('getDashboardData', { timeFrame: timeFrame }, { refresh: ['getDashboardData'] }),
+    getDashboardData: (timeFrame) => apexClass.mutate('getDashboardData', { timeFrame: timeFrame }, { refresh: ['getDashboardData'] }),
+    getDashboardData: (timeFrame) => apexClass.mutate('getDashboardData', { timeFrame: timeFrame }, { refresh: ['getDashboardData'] }),
+    getDashboardData: (timeFrame) => apexClass.mutate('getDashboardData', { timeFrame: timeFrame }, { refresh: ['getDashboardData'] }),
+    getDashboardData: (timeFrame) => apexClass.mutate('getDashboardData', { timeFrame: timeFrame }, { refresh: ['getDashboardData'] }),
+    getDashboardData: (timeFrame) => apexClass.mutate('getDashboardData', { timeFrame: timeFrame }, { refresh: ['getDashboardData'] }),
+    getDashboardData: (timeFrame) => apexClass.mutate('getDashboardData', { timeFrame: timeFrame }, { refresh: ['getDashboardData'] }),
+    getDashboardData: (timeFrame) => apexClass.mutate('getDashboardData', { timeFrame: timeFrame }, { refresh: ['getDashboardData'] }),
+    getDashboardData: (timeFrame) => apexClass.mutate('getDashboardData', { timeFrame: timeFrame }, { refresh: ['getDashboardData'] }),
+    getDashboardData: (timeFrame) => apexClass.mutate('getDashboardData', { timeFrame: timeFrame }, { refresh: ['getDashboardData'] }),
+    getDashboardData: (timeFrame) => apexClass.mutate('getDashboardData', { timeFrame: timeFrame }, { refresh: ['getDashboardData'] }),
+    getDashboardData: (timeFrame) => apexClass.mutate('getDashboardData', { timeFrame: timeFrame }, { refresh: ['getDashboardData'] }),
+    getDashboardData: (timeFrame) => apexClass.mutate('getDashboardData', { timeFrame: timeFrame }, { refresh: ['getDashboardData'] }),
+    getDashboardData: (timeFrame) => apexClass.mutate('getDashboardData', { timeFrame: timeFrame }, { refresh: ['getDashboardData'] }),
+    getDashboardData: (timeFrame) => apexClass.mutate('getDashboardData', { timeFrame: timeFrame }, { refresh: ['getDashboardData'] }),
+    getDashboardData: (timeFrame) => apexClass.mutate('getDashboardData', { timeFrame: timeFrame }, { refresh: ['getDashboardData'] }),
+    getDashboardData: (timeFrame) => apexClass.mutate('getDashboardData', { timeFrame: timeFrame }, { refresh: ['getDashboardData'] }),
+    getDashboardData: (timeFrame) => apexClass.query('getDashboardData', { timeFrame: timeFrame })
+  }), [apexClass]);
+}
